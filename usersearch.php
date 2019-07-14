08abaf08af2d9c63c0e83de6c44f661313038d4b
jeremias ramirez <jereramirez499@gmail.com>
2019-07-14 03:34:56 -0400

    agregando archivo php para mostrar los usuarios encontrados

---
+++ usersearch.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+if(!$_SESSION["name"]){
+			 
+	header("Location: out.php");
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Seach</title>
+	<link rel="stylesheet" href="styles.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="fontawesome-free-5.9.0-web/css/all.min.css">
+	<link rel="stylesheet" href="usersearch.css">
+</head>
+<body>
+	<header class="main__header">
+		<article class="main_main">
+			<a href="main.php" class="title fas fa-home"></a>
+		</article>
+
+		<article class="main_out">	
+		<a href="publication.php" class="user_perfil far fa-bell" title="Notification"></a>
+		<a href="user.php" class="user_perfil fas fa-user" title="User"></a> 
+		<a href="out.php" class="perfil_out fas fa-sign-out-alt" title="Close"></a>
+		</article>
+
+	</header>
+ <main class="main">
+ 	<div class="search__user">
+ 		<form action="usersearch.php" method="post">
+ 			<input type="search" class="search" placeholder="Search user">
+ 			<button class="btn__search fas fa-search"></button>
+ 		</form>
+ 	</div>
+
+<script src="usersearch.js"></script>
+</body>
+</html>
