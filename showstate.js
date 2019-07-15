8ccfce8b4b6fc24170ca61cd0cc6016d5d3dc864
jeremias ramirez <jereramirez499@gmail.com>
2019-07-14 22:59:11 -0400

    Agregando archivo de javascript de interaccion en estados timing

---
+++ showstate.js
@@ -0,0 +1,68 @@
+		let containerStates = document.getElementById("statesitem");
+		let itemStates = containerStates.querySelectorAll("#data_state--js");
+		if(containerStates && itemStates){
+
+		for(let item=0; item < itemStates.length; item++){
+			itemStates[item].addEventListener("click", (e)=>{
+				let targ = e.target.textContent
+
+				let contentSt = document.createElement("div");
+					contentSt.setAttribute("class","alls");
+					contentSt.setAttribute("id","containerAll");
+					document.body.insertAdjacentElement("afterbegin", contentSt);
+					
+					let contentEl = document.createElement("p");
+						contentEl.textContent = targ
+						contentEl.setAttribute("class", "data_state tops")
+
+						contentSt.appendChild(contentEl)
+
+					let timingContainer = document.createElement("div");
+						timingContainer.setAttribute("class", "timingContainer")
+						contentSt.insertAdjacentElement("afterbegin",timingContainer)
+						 
+					let timing = document.createElement("p");
+						timing.textContent = 1
+						timing.setAttribute("class", "timing")
+
+						timingContainer.insertAdjacentElement("afterbegin",timing)
+
+					let timingClose = document.createElement("span");
+						timingClose.textContent = "X"
+						timingClose.setAttribute("class", "timingClose");
+
+						contentSt.appendChild(timingClose)
+
+					let counterTiming = 1;
+
+					let intervalTiming = setInterval(()=>{
+
+							counterTiming += 1;
+							timing.textContent = counterTiming;
+
+							if(counterTiming > 10){
+						
+								contentSt.classList.add("goState");
+
+							setTimeout(()=>{
+								contentSt.remove()
+							},2000)
+								clearInterval(intervalTiming);
+							 
+							}
+
+					}, 800)
+						timingClose.addEventListener("click", (e)=>{
+							contentSt.classList.add("goState");
+							setTimeout(()=>{
+								contentSt.remove()
+							},2000)
+						})
+						contentSt.addEventListener("click", (e)=>{
+							// contentSt.remove()
+							counterTiming=0
+						})
+
+			}, false);
+		}
+	}
