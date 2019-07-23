41c69b71b789cf41947bcb32bf74f0b0710f0e20
Jeremias ramirez <jereramirez499@gmail.com>
2019-07-23 16:20:27 -0400

    Agregando ver imagen de perfil

--- public/js/showPerfiClick.js
+++ public/js/showPerfiClick.js
@@ -4,7 +4,7 @@
 
 class showPerfil{
 
-    createView(){
+    createView(imguser){
         let viewContainer = document.createElement("div");
             viewContainer.setAttribute("class", "showPerfil")
             viewContainer.setAttribute("id", "showPerfil")
@@ -13,17 +13,30 @@ class showPerfil{
             }
 
              let containerForm = document.createElement("div")
-                 containerForm.setAttribute("id", "containerForm")
-                 containerForm.setAttribute("class", "containerForm")
+                 containerForm.setAttribute("id", "containerPerfilClick")
+                 containerForm.setAttribute("class", "containerPerfilClick")
                  viewContainer.appendChild(containerForm);
 
           let closeView = document.createElement("p");
               closeView.textContent = "X";
-              closeView.setAttribute("class", "closeview")
-              closeView.setAttribute("id", "closeview")
+              closeView.setAttribute("class", "closeperfil")
+              closeView.setAttribute("id", "closeperfil")
               containerForm.insertAdjacentElement("beforebegin", closeView)
+            
 
-              closeview.addEventListener("click", (e)=>{
+              let containiMG = document.createElement("div");
+                containiMG.setAttribute("id", "containImg")
+                containiMG.setAttribute("class", "containImg")
+                containerForm.appendChild(containiMG);
+
+            let imgUser = document.createElement("img");
+                imgUser.setAttribute("class", "imgUser")
+                imgUser.setAttribute("src", imguser.src)
+                imgUser.setAttribute("id", "imgUser")
+                imgUser.setAttribute("class", "imgUser")
+                containiMG.appendChild(imgUser);
+
+              closeView.addEventListener("click", (e)=>{
                   let containerClose = document.getElementById("showPerfil");
                   if(containerClose){
                       containerClose.classList.add("goState")
@@ -41,8 +54,9 @@ class showPerfil{
 }
 let perfilimg = document.getElementById("perfilimg");
 perfilimg.addEventListener("click", (e)=>{
+        let imguser = e.target;
         let instance = new showPerfil();
-            instance.createView();
+            instance.createView(imguser);
 
     }, false);
 
