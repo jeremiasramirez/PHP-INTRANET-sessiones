46f1aa8f5d5eb361374e0c04468cf51056bb966c
Jeremias ramirez <jereramirez499@gmail.com>
2019-08-09 19:01:23 -0400

    reforzando seguridad en cambiado de foto de perfil

--- add/changeperfil.php
+++ add/changeperfil.php
@@ -3,23 +3,27 @@ session_start();
  
    include"../model/model.php";
 
-   	$id = $_SESSION["iduser"];
-   	print_r($_FILES);
-   	if((isset($_FILES["photoperfil"]["name"])) && $_FILES["photoperfil"]["error"]==UPLOAD_ERR_OK){
+   	if((isset($_FILES["photoperfil"]["name"])) && $_FILES["photoperfil"]["error"]==UPLOAD_ERR_OK 
+   		&& $_FILES["photoperfil"] != ""){
 
 		$destinate="../uploads/perfil/";
 		$nameimg = $_FILES["photoperfil"]["name"];
 		$nameimg = $id.rand(0,20000).'.png';
 
 		$destinate = $destinate . $nameimg;
-		if(move_uploaded_file($_FILES["photoperfil"]["tmp_name"], $destinate) && $_FILES["photoperfil"]["type"]!="png"){
+		if(move_uploaded_file($_FILES["photoperfil"]["tmp_name"], $destinate) && 
+			$_FILES["photoperfil"]["type"]!="mp4"){
 			mysqli_query($conection, "UPDATE usuario SET photo='$nameimg' WHERE user_id = '$id'");
-		  	header("Location: ../users.php");
+		  	header("Location: ../users.php?update=updated");
 
 		}
-		  	header("Location: ../users.php");
+		else{
+		  	header("Location: ../users.php?errorplatform=errorplatform");
+		}
 
  
+}else{
+	header("Location: ../users.php?erroruser=erroruser");
 }
  		
 ?>
