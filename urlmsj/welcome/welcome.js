f8942e6c0f774a2a5ae0318216fe7e058630b2f1
Jeremias ramirez <jereramirez499@gmail.com>
2019-08-12 22:17:41 -0400

    Agregado de archivo en la carpeta urlmsj de mensaje de bienvenida al perfil

---
+++ urlmsj/welcome/welcome.js
@@ -0,0 +1,17 @@
+
+     let timeErrorMessage = setTimeout(()=>{
+                        let deleteErrorMessage = document.getElementById("user__name");
+
+                        if(deleteErrorMessage){
+                            deleteErrorMessage.remove();
+                        }
+
+                    }, 3000);
+
+
+
+
+
+
+
+
---
+++ urlmsj/welcome/welcome.php
@@ -0,0 +1,25 @@
+<?php
+	session_start();
+
+
+	function welcomeuser($sexo, $girlman){
+
+		define("WOMEN", "femenino");
+		define("MAN", "masculino");
+
+		if($sexo==MAN){
+			print("<h1 class=user__name id=user__name>
+						<p class=emoji>😃</p>¡Bienvenido ".$girlman."!</h1>");
+		}
+		else if($sexo == WOMEN){
+			print("<h1 class=user__name>
+				<p class=emoji>😃</p>¡Bienvenida ".$girlman."!</h1>");
+		}
+		else{
+			print("<h1 class=user__name>
+				<p class=emoji>😃</p>¡Bienvenido!</h1>");
+		}
+}
+
+
+?>
