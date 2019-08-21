33529f265cfec8adad3857fc12b0df9f5e793d56
Jeremias ramirez <jereramirez499@gmail.com>
2019-08-21 10:27:47 -0400

    separando logica de publication

---
+++ views/publication-views.php
@@ -0,0 +1,21 @@
+<?php 
+require "model/model.php";
+
+class ShowState extends conectionDB{
+
+	function getAllStates(){
+		
+		parent::conected();
+		global $conection;
+
+		$statement_show = "SELECT stat FROM allstates";
+		$query_show = mysqli_query($conection, $statement_show);
+
+		while($row = mysqli_fetch_array($query_show)){
+			print("<p class=data_state id=data_state--js>".$row["stat"]."</p>");
+		}
+	}
+}
+
+$newshow = new ShowState;
+$newshow->getAllStates();
